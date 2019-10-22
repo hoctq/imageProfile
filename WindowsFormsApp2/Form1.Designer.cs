1aa618376e58e8bb8dedda0e05f1af41fdbbf28a
Hoc Trinh <t.q.hock56tt@gmail.com>
2019-10-23 01:31:05 +0700

    push project

---
+++ WindowsFormsApp2/Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace WindowsFormsApp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnChoose = new System.Windows.Forms.Button();
+            this.btnHandle = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.btnSave = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnChoose
+            // 
+            this.btnChoose.Location = new System.Drawing.Point(139, 693);
+            this.btnChoose.Name = "btnChoose";
+            this.btnChoose.Size = new System.Drawing.Size(75, 23);
+            this.btnChoose.TabIndex = 4;
+            this.btnChoose.Text = "Choose File";
+            this.btnChoose.UseVisualStyleBackColor = true;
+            this.btnChoose.Click += new System.EventHandler(this.btnChoose_Click);
+            // 
+            // btnHandle
+            // 
+            this.btnHandle.Enabled = false;
+            this.btnHandle.Location = new System.Drawing.Point(269, 693);
+            this.btnHandle.Name = "btnHandle";
+            this.btnHandle.Size = new System.Drawing.Size(75, 23);
+            this.btnHandle.TabIndex = 7;
+            this.btnHandle.Text = "Handle";
+            this.btnHandle.UseVisualStyleBackColor = true;
+            this.btnHandle.Click += new System.EventHandler(this.btnHandle_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(640, 640);
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(416, 693);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 8;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(661, 746);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnHandle);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btnChoose);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnChoose;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btnHandle;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.Button btnSave;
+    }
+}
+
---
+++ WindowsFormsApp2/Form1.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using System.Drawing.Imaging;
+//using AForge;
+//using AForge.Imaging;
+//using AForge.Math.Geometry;
+//using Point = System.Drawing.Point;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        string path;
+        Bitmap imageLast;
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+        private static Bitmap resizeImage(Image imgToResize, Size size)
+        {
+            int sourceWidth = imgToResize.Width;
+            int sourceHeight = imgToResize.Height;
+
+            float nPercent = 0;
+            float nPercentW = 0;
+            float nPercentH = 0;
+
+            nPercentW = ((float)size.Width / (float)sourceWidth);
+            nPercentH = ((float)size.Height / (float)sourceHeight);
+
+            //if (nPercentH < nPercentW)
+            //    nPercent = nPercentH;
+            //else
+                nPercent = nPercentW;
+
+            int destWidth = (int)(sourceWidth * nPercent);
+            int destHeight = (int)(sourceHeight * nPercent);
+
+            Bitmap b = new Bitmap(destWidth, destHeight);
+            Graphics g = Graphics.FromImage(b);
+            g.Clear(Color.White);
+            g.InterpolationMode = InterpolationMode.HighQualityBicubic;
+
+            g.DrawImage(imgToResize, 0, 0, destWidth, destHeight);
+            g.Dispose();
+
+            return b;
+        }
+
+        private static Bitmap cropImage(Image img, Rectangle cropArea)
+        {
+            try
+            {
+                Bitmap bmpImage = new Bitmap(img);
+                Bitmap bmpCrop = bmpImage.Clone(cropArea, bmpImage.PixelFormat);
+                return bmpCrop;
+            }
+            catch (System.OutOfMemoryException e)
+            {
+                MessageBox.Show(e.Message);
+                return null;
+            }
+        }
+
+        private Bitmap setFinalSizeImage(Bitmap img) {
+            Bitmap bitmap = new Bitmap(640, 640);
+            Graphics g = Graphics.FromImage(bitmap);
+            g.Clear(Color.White);
+            g.DrawImage(img, new Point(0, 0));
+            g.Dispose();
+            return bitmap;
+        }
+
+        private Bitmap rotateImage(Bitmap b, float angle)
+        {
+            //create a new empty bitmap to hold rotated image
+            Bitmap returnBitmap = new Bitmap(b.Width, b.Height);
+            //make a graphics object from the empty bitmap
+            Graphics g = Graphics.FromImage(returnBitmap);
+            //move rotation point to center of image
+            g.TranslateTransform((float)b.Width / 2, (float)b.Height / 2);
+            //rotate
+            g.RotateTransform(angle);
+            //move image back
+            g.TranslateTransform(-(float)b.Width / 2, -(float)b.Height / 2);
+            //draw passed in image onto graphics object
+            g.DrawImage(b, new Point(0, 0));
+            return returnBitmap;
+        }
+
+        private void btnHandle_Click(object sender, EventArgs e)
+        {
+            if (path == null || path.Length == 0) {
+                MessageBox.Show("Please choose image before");
+                return;
+            }
+            Image img = Image.FromFile(path);
+            Size size = new Size(640, 640);
+            Rectangle rect = new Rectangle(0, 210, 640, 440);
+
+            Bitmap image = resizeImage(img, size);
+            Bitmap imageCrop = cropImage(image, rect);
+
+            Bitmap imageFinalSize = setFinalSizeImage(imageCrop);
+
+            imageLast = rotateImage(imageFinalSize, 270);
+            pictureBox1.Image = imageLast;
+        }
+
+        private void btnChoose_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.Filter = "Image files (*.jpg, *.jpeg, *.png) | *.jpg; *.jpeg; *.png";
+            openFileDialog1.InitialDirectory = @"C:\";
+            openFileDialog1.ShowDialog();
+            
+            path = openFileDialog1.FileName;
+            if (path != null || path.Length > 0) {
+                btnHandle.Enabled = true;
+                pictureBox1.Image = Image.FromFile(path);
+            }
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            saveFileDialog1.Filter = "Images|*.png;*.jpeg;*.jpg";
+            saveFileDialog1.InitialDirectory = @"C:\";
+            ImageFormat format = ImageFormat.Png;
+            if (saveFileDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                string ext = System.IO.Path.GetExtension(saveFileDialog1.FileName);
+                string pathSave = saveFileDialog1.FileName;
+                switch (ext)
+                {
+                    case ".jpg":
+                        format = ImageFormat.Jpeg;
+                        break;
+                    case ".png":
+                        format = ImageFormat.Png;
+                        break;
+                }
+                imageLast.Save(saveFileDialog1.FileName, format);
+                path = "";
+                imageLast = null;
+                btnHandle.Enabled = false;
+            }
+        }
+
+    }
+}
